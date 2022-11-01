a28b0e871791b4e1de23c298609517100b422396
nalinsb <nalinsanjeewa1997@gmail.com>
2022-11-01 16:29:18 +0530

    Initial Commit

--- src/main/java/lk/ijse/dep9/api/CustomerServlet.java
+++ src/main/java/lk/ijse/dep9/api/CustomerServlet.java
@@ -70,7 +70,7 @@ public class CustomerServlet extends HTTPServlet2 {
 
     @Override
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("doDelete");
+
     }
 
     @Override
